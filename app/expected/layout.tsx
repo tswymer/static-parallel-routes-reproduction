ad4f9ea9ff0ef424510b8355498cb6ab9e34a3cc
Tobias Wymer <tobi.wymer@googlemail.com>
2024-09-24 09:27:24 +0200

    Added more tests

---
+++ app/expected/layout.tsx
@@ -0,0 +1,26 @@
+export default function TestLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const renderTime = new Date().toISOString();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        backgroundColor: "coral",
+        padding: "20px",
+      }}
+    >
+      <div>
+        <h1>Layout (expected to be static)</h1>
+        <p>Rendered at: {renderTime}</p>
+      </div>
+
+      {children}
+    </div>
+  );
+}
---
+++ app/expected/page.tsx
@@ -0,0 +1,15 @@
+import { headers } from "next/headers";
+
+export default function ParallelRoute() {
+  const renderTime = new Date().toISOString();
+
+  const country = headers().get("X-Vercel-IP-Country");
+
+  return (
+    <div style={{ backgroundColor: "green", padding: "10px" }}>
+      <h3>Dynamic Parallel Route (dynamic)</h3>
+      <p>Rendered at: {renderTime}</p>
+      <p>X-Vercel-IP-Country header (causing dynamic rendering): {country}</p>
+    </div>
+  );
+}
